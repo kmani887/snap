f42761695ad5d4ca2c16eefdc053a1cebec54db4
amongash <amongash08@gmail.com>
2022-02-15 11:59:32 +0300

    Removed unused files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./container/Home";
+import Login from "./components/Login";
 
-import './App.css';
-import{Routes,Route,useNavigate} from'react-router-dom'
-import  Login from './components/Login';
-import Home from './container/Home';
 function App() {
-  return (
- <Routes>
-   <Route path="login" element={<Login />} />
-   <Route path="/*" element={<Home />} />
- </Routes>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="login" element={<Login />} />
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
